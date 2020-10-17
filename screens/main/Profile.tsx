a225f16e9bd116202577f248a93a2c2e3f13d141
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-10-18 02:16:17 +0300

    refactor profile page

--- screens/main/Profile.tsx
+++ screens/main/Profile.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { MainScreenProps } from '../../types/navigation'
 import { Layout, Text, Divider, Icon, ListItem, List } from '@ui-kitten/components'
@@ -20,31 +20,21 @@ export function Profile(props: MainScreenProps<'Profile'>) {
 
   return (
     <Layout style={styles.container}>
-      <View style={styles.panel}>
-        <Text category="h4" style={{ fontWeight: '500', marginBottom: 6, color: '#858585' }}>
-          Пользователь
-        </Text>
-        <Divider style={{ marginTop: 10 }} />
-
+      <Group title="Пользователь">
         <DataItem label="Фамилия" info={lastName} />
         <DataItem label="Имя" info={firstName} />
         <DataItem label="Отчество" info={patronymic} />
         <DataItem label="Прочие данные" info={user.otherInfo} />
-      </View>
+      </Group>
 
-      <View style={{ ...styles.panel, minHeight: 220, justifyContent: 'flex-start', flex: 0 }}>
-        <Text style={{ fontSize: 22, fontWeight: '500', marginBottom: 6, color: '#858585' }}>
-          Удостоверения личности
-        </Text>
-        <Divider style={{ marginTop: 10 }} />
-
-        <ListAccessoriesShowcase />
-      </View>
+      <Group title="Удостоверения личности">
+        <VCList />
+      </Group>
     </Layout>
   )
 }
 
-const ListAccessoriesShowcase = () => {
+const VCList = () => {
   const passport = [{ title: 'Выдан: Почта России', description: 'Тип документа: Паспорт РФ' }]
 
   const renderItemIcon = (props: any) => <Icon {...props} name="person" />
@@ -54,6 +44,19 @@ const ListAccessoriesShowcase = () => {
   )
 
   return <List style={{ backgroundColor: 'white' }} data={passport} renderItem={renderItem} />
+}
+
+function Group({ title, children }: PropsWithChildren<{ title: string }>) {
+  return (
+    <View style={styles.panel}>
+      <Text category="h4" style={styles.title}>
+        {title}
+      </Text>
+      <Divider />
+
+      {children}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -67,6 +70,11 @@ const styles = StyleSheet.create({
     padding: 12,
     borderRadius: 4,
     marginBottom: 12,
+  },
+  title: {
+    fontWeight: '500',
+    marginBottom: 16,
+    color: '#858585',
   },
 })
 
